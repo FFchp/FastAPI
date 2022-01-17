e49904df9aca0ae345556f78439eeb10b1876008
FFchp <chumpod.usa@spumail.net>
2022-01-17 13:55:58 +0700

    comment

--- main.py
+++ main.py
@@ -37,25 +37,26 @@ def update_id(id, request: schemas.Update_User, db : Session = Depends(get_db)):
     return 'done'
 
 # Qurry all User
-#@app.get('/user', status_code= status.HTTP_200_OK, response_model = schemas.ShowUser, tags = ['user'])
-#def querry(db : Session = Depends(get_db)):
-#    users = db.query(models.User).order_by(models.User.id).all()
-#    return users
+'''
+@app.get('/user', status_code= status.HTTP_200_OK, response_model = schemas.ShowUser, tags = ['user'])
+def querry(db : Session = Depends(get_db)):
+    users = db.query(models.User).order_by(models.User.id).all()
+    return users'''
 
 # Querry User By id
-#@app.get('/user/{id}', status_code=status.HTTP_200_OK, response_model = schemas.ShowUser, tags = ['user'])
-#def show(id, response : Response, db : Session = Depends(get_db)):
-#    user = db.query(models.User).filter(models.User.id == id).first()
-#    if not user:
-#        # response.status_code = status.HTTP_404_NOT_FOUND
-#        # return {'detail' : f'blog with the id {id} is not aviavle'}
-#        raise HTTPException(status_code = status.HTTP_404_NOT_FOUND,
-#                            detail = f'user with the id {id} is not aviable')
-#    return user
+'''@app.get('/user/{id}', status_code=status.HTTP_200_OK, response_model = schemas.ShowUser, tags = ['user'])
+def show(id, response : Response, db : Session = Depends(get_db)):
+    user = db.query(models.User).filter(models.User.id == id).first()
+    if not user:
+        # response.status_code = status.HTTP_404_NOT_FOUND
+        # return {'detail' : f'blog with the id {id} is not aviavle'}
+        raise HTTPException(status_code = status.HTTP_404_NOT_FOUND,
+                            detail = f'user with the id {id} is not aviable')
+    return user'''
 
 # Delete User
-#@app.delete('/user/{id}', status_code = status.HTTP_204_NO_CONTENT, tags = ['user'])
-#def delete(id, db: Session = Depends(get_db)):
-#    db.query(models.User).filter(models.User.id == id).delete(synchronize_session=False)
-#    db.commit()
-#    return 'done'
+'''@app.delete('/user/{id}', status_code = status.HTTP_204_NO_CONTENT, tags = ['user'])
+def delete(id, db: Session = Depends(get_db)):
+    db.query(models.User).filter(models.User.id == id).delete(synchronize_session=False)
+    db.commit()
+    return 'done'''
